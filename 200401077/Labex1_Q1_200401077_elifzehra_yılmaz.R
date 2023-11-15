143701083d5788246065873fa2db38cadd4c49a9
Elifzyilmaz <elifzehralala@gamil.com>
2023-11-15 22:44:40 +0300

    1.5, testleri ile tamamlandı.

---
+++ 200401077/Labex1_Q1_200401077_elifzehra_yılmaz.R
@@ -0,0 +1,8 @@
+dosya_url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+dosya_isim <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(dosya_url, destfile = dosya_isim, method = "auto")
+maps <- read.csv(dosya_isim, sep = ";", header = TRUE)
+print(dim(maps))
+maps$Latitude <- as.numeric(gsub("N", "", maps$Latitude))
+idx <- grep("W", maps$Longitude)
+maps$Longitude <- as.numeric(gsub("[EW]", "", maps$Longitude))
